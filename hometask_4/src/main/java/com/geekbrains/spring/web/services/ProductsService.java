2331b605f0e422dd9f76c2dd4707146158b61075
krllgglv <kirillgogolev@yandex.ru>
2022-01-19 14:26:15 +0300

    source project

---
+++ hometask_4/src/main/java/com/geekbrains/spring/web/services/ProductsService.java
@@ -0,0 +1,65 @@
+package com.geekbrains.spring.web.services;
+
+import com.geekbrains.spring.web.dto.ProductDto;
+import com.geekbrains.spring.web.entities.Category;
+import com.geekbrains.spring.web.entities.Product;
+import com.geekbrains.spring.web.exceptions.ResourceNotFoundException;
+import com.geekbrains.spring.web.repositories.ProductsRepository;
+import com.geekbrains.spring.web.repositories.specifications.ProductsSpecifications;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductsService {
+    private final ProductsRepository productsRepository;
+    private final CategoriesService categoriesService;
+
+    @Transactional
+    public Page<Product> findAll(Integer minPrice, Integer maxPrice, String partTitle, Integer categoryId, Integer page) {
+        Specification<Product> spec = Specification.where(null);
+        if (minPrice != null) {
+            spec = spec.and(ProductsSpecifications.priceGreaterOrEqualsThan(minPrice));
+        }
+        if (maxPrice != null) {
+            spec = spec.and(ProductsSpecifications.priceLessThanOrEqualsThan(maxPrice));
+        }
+        if (partTitle != null) {
+            spec = spec.and(ProductsSpecifications.titleLike(partTitle));
+        }
+        if (categoryId != null) {
+            Category category = categoriesService.findOneById(categoryId).orElseThrow(() -> new ResourceNotFoundException("No such category"));
+            spec = spec.and(ProductsSpecifications.categoryEquals(category));
+        }
+
+
+
+        return productsRepository.findAll(spec, PageRequest.of(page - 1, 8));
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productsRepository.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        productsRepository.deleteById(id);
+    }
+
+    public Product save(Product product) {
+        return productsRepository.save(product);
+    }
+
+    @Transactional
+    public Product update(ProductDto productDto) {
+        Product product = productsRepository.findById(productDto.getId()).orElseThrow(() -> new ResourceNotFoundException("Невозможно обновить продукта, не надйен в базе, id: " + productDto.getId()));
+        product.setPrice(productDto.getPrice());
+        product.setTitle(productDto.getTitle());
+        return product;
+    }
+}

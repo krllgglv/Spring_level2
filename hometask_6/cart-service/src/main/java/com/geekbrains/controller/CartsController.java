1bb2800e94f8c2fdbfa98e0157ecaca57518afe7
krllgglv <kirillgogolev@yandex.ru>
2022-01-31 14:42:44 +0300

    added dependencies

---
+++ hometask_6/cart-service/src/main/java/com/geekbrains/controller/CartsController.java
@@ -0,0 +1,66 @@
+package com.geekbrains.controller;
+
+import com.geekbrains.dto.Cart;
+import com.geekbrains.spring.web.api.dto.StringResponse;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import com.geekbrains.service.CartService;
+
+@RestController
+@RequestMapping("/api/v1/cart")
+@RequiredArgsConstructor
+@EnableEurekaClient
+public class CartsController {
+    private final CartService cartService;
+    private final RestTemplate restTemplate;
+
+    @GetMapping("/{uuid}")
+    public Cart getCart(@RequestHeader(required = false) String username, @PathVariable String uuid) {
+        return cartService.getCurrentCart(getCurrentCartUuid(username, uuid));
+    }
+
+    @GetMapping("/generate")
+    public StringResponse getCart() {
+        return new StringResponse(cartService.generateCartUuid());
+    }
+
+    @GetMapping("/{uuid}/add/{productId}")
+    public void add(@RequestHeader(required = false) String username, @PathVariable String uuid, @PathVariable Long productId) {
+//        restTemplate.getForObject()
+        // TODO: 31.01.2022 добавить получение продукта из продуктового сервиса
+        cartService.addToCart(getCurrentCartUuid(username, uuid), productId);
+    }
+
+    @GetMapping("/{uuid}/decrement/{productId}")
+    public void decrement(@RequestHeader(required = false) String username, @PathVariable String uuid, @PathVariable Long productId) {
+        cartService.decrementItem(getCurrentCartUuid(username, uuid), productId);
+    }
+
+    @GetMapping("/{uuid}/remove/{productId}")
+    public void remove(@RequestHeader(required = false) String username, @PathVariable String uuid, @PathVariable Long productId) {
+        cartService.removeItemFromCart(getCurrentCartUuid(username, uuid), productId);
+    }
+
+    @GetMapping("/{uuid}/clear")
+    public void clear(@RequestHeader(required = false) String username, @PathVariable String uuid) {
+        cartService.clearCart(getCurrentCartUuid(username, uuid));
+    }
+
+    @GetMapping("/{uuid}/merge")
+    public void merge(@RequestHeader(required = false) String username, @PathVariable String uuid) {
+        cartService.merge(
+                getCurrentCartUuid(username, null),
+                getCurrentCartUuid(null, uuid)
+        );
+    }
+
+    private String getCurrentCartUuid(String username, String uuid) {
+        if (username != null) {
+            return cartService.getCartUuidFromSuffix(username);
+        }
+        return cartService.getCartUuidFromSuffix(uuid);
+    }
+}
